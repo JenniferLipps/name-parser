6e90eaff236867b6f1cdf61dffdd5436fc05c4da
Jennifer Lipps <jjlipps@gmail.com>
2019-09-14 11:18:19 -0500

    first tests completed and passed. Practice with Red Green Refactor.

--- NameParser.Tests/UnitTest1.cs
+++ NameParser.Tests/UnitTest1.cs
@@ -1,14 +1,39 @@
 using System;
 using Xunit;
+using NameParser;
 
 namespace NameParser.Tests
 {
-    public class UnitTest1
+    public class NameParserTests
     {
         [Fact]
-        public void Test1()
+        public void FirstAndLastNameShouldParseAccurately()
         {
+            //Arrange
+            var name = "Martin Cross";
+            var parser = new Parser();
 
+            //Act
+            var result = parser.ParseName(name);
+
+            //Assert
+            Assert.Equal("Martin", result.FirstName);
+            Assert.Equal("Cross", result.LastName);
+        }
+
+        [Fact]
+        public void ThreePartNamesShouldBeParsedAccurately()
+        {
+            //Arrange
+            var name = "Martin Zachariah Cross";
+            var parser = new Parser();
+
+            //Act
+            var result = parser.ParseName(name);
+
+            //Assert
+            Assert.Equal("Martin", result.FirstName);
+            Assert.Equal("Cross", result.LastName);
         }
     }
 }
